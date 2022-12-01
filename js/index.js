d9ce5f249d3195d2020535fcef9ca05df282f0d6
Renato Tonelli de Souza <renato.souza1969@yahoo.com.br>
2022-11-30 22:07:25 -0300

    Esta versao esta bem melhor

---
+++ js/index.js
@@ -0,0 +1,76 @@
+
+function btnFacilitaBus() {
+    let pontos = document.getElementById("pontos");
+    let maisTexto = document.getElementById("mais");
+    let btnLeiaMais = document.getElementById("btnLeiaMais");
+
+    if (pontos.style.display == "none") {
+
+        pontos.style.display = "inline";
+        maisTexto.style.display = "none";
+        btnLeiaMais.innerHTML = "Leia Mais";
+
+    } else {
+
+        pontos.style.display = "none";
+        maisTexto.style.display = "inline";
+        btnLeiaMais.innerHTML = "Leia Menos";
+    }
+}
+
+function btnQualABoa() {
+    let pontos = document.getElementById("pontosDois");
+    let maisTexto = document.getElementById("maisDois");
+    let btnLeiaMais = document.getElementById("btnLeiaMaisDois");
+
+    if (pontos.style.display == "none") {
+
+        pontos.style.display = "inline";
+        maisTexto.style.display = "none";
+        btnLeiaMais.innerHTML = "Leia Mais";
+
+    } else {
+
+        pontos.style.display = "none";
+        maisTexto.style.display = "inline";
+        btnLeiaMais.innerHTML = "Leia Menos";
+    }
+}
+
+function btnEncantoSenac() {
+    let pontos = document.getElementById("pontosTres");
+    let maisTexto = document.getElementById("maisTres");
+    let btnLeiaMais = document.getElementById("btnLeiaMaisTres");
+
+    if (pontos.style.display == "none") {
+
+        pontos.style.display = "inline";
+        maisTexto.style.display = "none";
+        btnLeiaMais.innerHTML = "Leia Mais";
+
+    } else {
+
+        pontos.style.display = "none";
+        maisTexto.style.display = "inline";
+        btnLeiaMais.innerHTML = "Leia Menos";
+    }
+}
+
+function btnBoraMarcar() {
+    let pontos = document.getElementById("pontosQuatro");
+    let maisTexto = document.getElementById("maisQuatro");
+    let btnLeiaMais = document.getElementById("btnLeiaMaisQuatro");
+
+    if (pontos.style.display == "none") {
+
+        pontos.style.display = "inline";
+        maisTexto.style.display = "none";
+        btnLeiaMais.innerHTML = "Leia Mais";
+
+    } else {
+
+        pontos.style.display = "none";
+        maisTexto.style.display = "inline";
+        btnLeiaMais.innerHTML = "Leia Menos";
+    }
+}

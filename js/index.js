45867c579115e7d658d2d898f1ed167b466f2a84
maycanin <maysoninacio@gmail.com>
2023-02-01 20:41:52 -0300

    inclusão projeto QualyLab e renomeação CR-Med

--- js/index.js
+++ js/index.js
@@ -75,7 +75,7 @@ function btnBoraMarcar() {
     }
 }
 
-function btnCcr() {
+function btnCrMed() {
     let pontos = document.getElementById("pontos");
     let maisTexto = document.getElementById("mais");
     let btnLeiaMais = document.getElementById("btnLeiaMais");
@@ -117,6 +117,25 @@ function btnCmv() {
     let pontos = document.getElementById("pontosTres");
     let maisTexto = document.getElementById("maisTres");
     let btnLeiaMais = document.getElementById("btnLeiaMaisTres");
+
+    if (pontos.style.display == "none") {
+
+        pontos.style.display = "inline";
+        maisTexto.style.display = "none";
+        btnLeiaMais.innerHTML = "Leia Mais";
+
+    } else {
+
+        pontos.style.display = "none";
+        maisTexto.style.display = "inline";
+        btnLeiaMais.innerHTML = "Leia Menos";
+    }
+}
+
+function btnQualyLab() {
+    let pontos = document.getElementById("pontosQuatro");
+    let maisTexto = document.getElementById("maisQuatro");
+    let btnLeiaMais = document.getElementById("btnLeiaMaisQuatro");
 
     if (pontos.style.display == "none") {
 
